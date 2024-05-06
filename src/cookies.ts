26bb29b5ad779c8d9c7c2a518031fe2c1a7213b1
Dylan Beattie <dylan@dylanbeattie.net>
2024-05-06 12:45:50 +0100

    Fix TS cookie handling.

--- src/cookies.ts
+++ src/cookies.ts
@@ -1,32 +1,18 @@
-/*
- * General utils for managing cookies in Typescript.
- */
-export function setCookie(name: string, val: string) {
-    const date = new Date();
-    const value = val;
-
-    // Set it expire in 7 days
-    date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
-
-    // Set it
-    document.cookie = name+"="+value+"; expires="+date.toUTCString()+"; path=/";
+export function setCookie(name: string, value: string) {
+    const expires = new Date();
+	const SEVEN_DAYS_IN_MILLISECONDS = 7 * 24 * 60 * 60 * 1000;
+    expires.setTime(expires.getTime() + SEVEN_DAYS_IN_MILLISECONDS);
+    document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/`;
 }
 
 export function getCookie(name: string) {
     const value = "; " + document.cookie;
-    const parts = value.split("; " + name + "=");
-
-    if (parts.length == 2) {
-        return parts.pop().split(";").shift();
-    }
+    const parts = value.split(`; ${name}=`);
+    return parts.pop()?.split(";").shift();
 }
 
 export function deleteCookie(name: string) {
-    const date = new Date();
-
-    // Set it expire in -1 days
-    date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
-
-    // Set it
-    document.cookie = name+"=; expires="+date.toUTCString()+"; path=/";
+    const expires = new Date();
+    expires.setTime(expires.getTime() + (-1 * 24 * 60 * 60 * 1000));
+    document.cookie = `${name}=; expires=${expires.toUTCString()}; path=/`;
 }
